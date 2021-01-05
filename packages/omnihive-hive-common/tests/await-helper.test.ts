528fb3e35fe1c1921d657d8c2f2e7aa4c331000c
Kaleb <kaleb.matthews@withone.vision>
2021-01-05 15:32:23 -0500

    Common Tests

---
+++ packages/omnihive-hive-common/tests/await-helper.test.ts
@@ -0,0 +1,34 @@
+import { AwaitHelper } from '@withonevision/omnihive-hive-common/helpers/AwaitHelper';
+import { assert } from 'chai';
+import { serializeError } from 'serialize-error';
+
+describe("AwaitHelper Tests", async function() {
+    const testString = "Successful Test";
+
+    const successFunction = async function (milliseconds: number): Promise<string> {
+        return new Promise<string>(resolve => setTimeout(() => resolve(testString), milliseconds));
+    }
+
+    const failFunction = async function (milliseconds: number): Promise<string> {
+        return new Promise<string>((_resolve, reject) => setTimeout(() => reject(testString), milliseconds));
+    }
+
+    it("standard call", async function(){
+        try{
+            const result = await AwaitHelper.execute<string>(successFunction(50));
+            assert.equal(result, testString);
+
+        } catch(err) {
+            throw new Error(JSON.stringify(serializeError(err)));
+        }
+    });
+
+    it("failure call", async function(){
+        try{
+            await AwaitHelper.execute<string>(failFunction(50));
+            assert.fail("Function should have failed");
+        } catch(err) {
+            assert.equal(err, testString);
+        }
+    });
+});

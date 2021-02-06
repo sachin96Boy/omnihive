1f89854e028add2d111ef5aad1070bd47d4b8a34
Doug Shontz <doug.shontz@withone.vision>
2021-02-06 10:47:25 -0500

    Forgot to commit NextJs stuff

---
+++ src/packages/omnihive/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    darkMode: false, // or 'media' or 'class'
+    plugins: [],
+    purge: ["./pages/**/*.js", "./components/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+};

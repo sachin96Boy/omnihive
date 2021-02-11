83e1f271333028f91208889beb61a361796dbb16
Kaleb <kaleb.matthews@withone.vision>
2021-02-11 16:08:50 -0500

    First 12Stone specific worker!

---
+++ src/custom/omnihive-worker-12stone/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  tabWidth: 4,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "es5",
+};
---
+++ src/custom/omnihive-worker-12stone/lib/models/WatchModels.ts
@@ -0,0 +1,11 @@
+import { Dayjs } from 'dayjs';
+
+export interface WatchContent {
+    id: number;
+    duration: string;
+    date?: Dayjs;
+    description: string;
+    poster: string;
+    title: string;
+    url: string;
+}

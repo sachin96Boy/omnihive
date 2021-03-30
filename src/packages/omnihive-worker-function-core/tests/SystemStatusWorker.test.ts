46bf4375644ea9f23e42befe788d000cc438816e
Jon Webb <jon@jonwebb.dev>
2021-03-30 11:03:14 -0400

    Switch to assert library to match existing tests

--- src/packages/omnihive-worker-function-core/tests/SystemStatusWorker.test.ts
+++ src/packages/omnihive-worker-function-core/tests/SystemStatusWorker.test.ts
@@ -1,5 +1,5 @@
 import { WorkerGetterBase } from "@withonevision/omnihive-core/models/WorkerGetterBase";
-import { expect } from "chai";
+import { assert } from "chai";
 import sinon from "sinon";
 import { TestService } from "../../../tests/services/TestService";
 import { TestConfigSettings } from "../../../tests/models/TestConfigSettings";
@@ -28,7 +28,7 @@ const {
 } = <TestConfigSettings>testService.getTestConfig("@withonevision/omnihive-worker-token-jsonwebtoken");
 tokenWorker.init(config);
 
-describe("system access token worker tests", () => {
+describe("system status worker tests", () => {
     before(() => {
         global.omnihive = {
             adminServer: undefined!,
@@ -76,54 +76,54 @@ describe("system access token worker tests", () => {
         it("execute - no token worker", async () => {
             try {
                 await worker.execute(undefined, "", undefined);
-                throw new Error("Method expected to fail, but didn't");
+                assert.fail("Method expected to fail, but didn't");
             } catch (err) {
-                expect(err).to.be.an("error").with.property("message", "Token Worker cannot be found");
+                assert.equal(err.message, "Token Worker cannot be found");
             }
         });
         it("execute - no headers", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             const result = await worker.execute(undefined, "", undefined);
-            expect(result.status).to.eq(400);
-            expect(result.response).to.have.nested.property("error.message", "Request Denied");
+            assert.equal(result.status, 400);
+            assert.nestedPropertyVal(result.response, "error.message", "Request Denied");
         });
         it("execute - no body", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             const result = await worker.execute({ ohAccess: "mockToken" }, "", undefined);
-            expect(result.status).to.eq(400);
-            expect(result.response).to.have.nested.property("error.message", "Request Denied");
+            assert.equal(result.status, 400);
+            assert.nestedPropertyVal(result.response, "error.message", "Request Denied");
         });
         it("execute - no ohAccess token", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             const result = await worker.execute({ mockHeader: "mockValue" }, "", {});
-            expect(result.status).to.eq(400);
-            expect(result.response).to.have.nested.property("error.message", "[ohAccessError] Token Invalid");
+            assert.equal(result.status, 400);
+            assert.nestedPropertyVal(result.response, "error.message", "[ohAccessError] Token Invalid");
         });
         it("execute - no admin password", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             const result = await worker.execute({ ohAccess: "mockToken" }, "", {});
-            expect(result.status).to.eq(400);
-            expect(result.response).to.have.nested.property("error.message", "Request Denied");
+            assert.equal(result.status, 400);
+            assert.nestedPropertyVal(result.response, "error.message", "Request Denied");
         });
         it("execute - incorrect admin password", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             sinon.stub(worker, "checkObjectStructure").returns({ adminPassword: "mockPassword" });
             const result = await worker.execute({ ohAccess: "mockToken" }, "", { adminPassword: "mockPassword" });
-            expect(result.status).to.eq(400);
-            expect(result.response).to.have.nested.property("error.message", "Request Denied");
+            assert.equal(result.status, 400);
+            assert.nestedPropertyVal(result.response, "error.message", "Request Denied");
         });
         it("execute", async () => {
             sinon.stub(WorkerGetterBase.prototype, "getWorker").returns(tokenWorker);
             sinon.stub(worker, "checkObjectStructure").returns({ adminPassword: "correctPassword" });
             const result = await worker.execute({ ohAccess: "mockToken" }, "", { adminPassword: "correctPassword" });
-            expect(result.status).to.eq(200);
-            expect(result.response).to.have.property("status", ServerStatus.Online);
-            expect(result.response).to.have.property("error", undefined);
+            assert.equal(result.status, 200);
+            assert.nestedPropertyVal(result.response, "status", ServerStatus.Online);
+            assert.nestedPropertyVal(result.response, "error", undefined);
         });
         it("getSwaggerDefinition", () => {
             const result = worker.getSwaggerDefinition();
-            expect(result?.definitions).to.be.an("object");
-            expect(result?.paths).to.be.an("object");
+            assert.isObject(result?.definitions);
+            assert.isObject(result?.paths);
         });
     });
 });

a6f92589636b0f45b7f9a896b2a62dd7416106c8
Doug Shontz <doug.shontz@withone.vision>
2021-07-16 06:08:40 -0400

    chore(monorepo): move graphbuilder to 12Stone
    Moving graph builder v1 to 12Stone and using v2 as the default.  Will not use v1 for main WOV branch anymore.

--- src/packages/omnihive-worker-graphql-builder-v1/enum/WhereModes.ts
+++
@@ -1,4 +0,0 @@
-export enum WhereMode {
-    All = "all",
-    Specific = "specific",
-}
--- src/packages/omnihive-worker-graphql-builder-v1/models/ConverterDatabaseTable.ts
+++
@@ -1,6 +0,0 @@
-export class ConverterDatabaseTable {
-    public index: number = 0;
-    public graphPath: string = "";
-    public tableName: string = "";
-    public tableAlias: string = "";
-}

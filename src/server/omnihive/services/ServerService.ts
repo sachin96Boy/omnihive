63605b883e9e2292e259f82661431306eb1cf352
Kaleb Matthews <kaleb.matthews@withone.vision>
2022-03-22 09:24:55 -0400

    fix(server): server boot now properly inits graph builders

--- src/server/omnihive/services/ServerService.ts
+++ src/server/omnihive/services/ServerService.ts
@@ -11,11 +11,16 @@ import { AwaitHelper } from "@withonevision/omnihive-core/helpers/AwaitHelper";
 import { IsHelper } from "@withonevision/omnihive-core/helpers/IsHelper";
 import { ObjectHelper } from "@withonevision/omnihive-core/helpers/ObjectHelper";
 import { ILogWorker } from "@withonevision/omnihive-core/interfaces/ILogWorker";
+import { IDatabaseWorker } from "@withonevision/omnihive-core/interfaces/IDatabaseWorker";
 import { IRestEndpointWorker } from "@withonevision/omnihive-core/interfaces/IRestEndpointWorker";
 import { IServerWorker } from "@withonevision/omnihive-core/interfaces/IServerWorker";
 import { HiveWorkerMetadataRestFunction } from "@withonevision/omnihive-core/models/HiveWorkerMetadataRestFunction";
 import { RegisteredHiveWorker } from "@withonevision/omnihive-core/models/RegisteredHiveWorker";
+import { HiveWorkerMetadataDatabase } from "@withonevision/omnihive-core/models/HiveWorkerMetadataDatabase";
+import { HiveWorkerMetadataGraphBuilder } from "@withonevision/omnihive-core/models/HiveWorkerMetadataGraphBuilder";
 import { RestEndpointExecuteResponse } from "@withonevision/omnihive-core/models/RestEndpointExecuteResponse";
+import { IGraphBuildWorker } from "@withonevision/omnihive-core/interfaces/IGraphBuildWorker";
+import { TableSchema } from "@withonevision/omnihive-core/models/TableSchema";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
@@ -29,10 +34,18 @@ import { CommonService } from "./CommonService";
 import { ApolloServerExpressConfig, ApolloServer } from "apollo-server-express";
 import { StringBuilder } from "@withonevision/omnihive-core/helpers/StringBuilder";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { mergeSchemas } from "@graphql-tools/schema";
 import { transformSync } from "esbuild";
 import Module from "module";
 import { nanoid } from "nanoid";
 import { runInNewContext } from "vm";
+import { ConnectionSchema } from "@withonevision/omnihive-core/models/ConnectionSchema";
+import { camelCase } from "../helpers/CamelCase";
+
+type BuilderDatabaseWorker = {
+    registeredWorker: RegisteredHiveWorker;
+    builderName: string;
+};
 
 export class ServerService {
     private webRootUrl: string = "";
@@ -92,6 +105,9 @@ export class ServerService {
                 app = await AwaitHelper.execute((server.instance as IServerWorker).buildServer(app));
                 logWorker?.write(OmniHiveLogLevel.Info, `Server Worker ${server.name} => Build Complete`);
             }
+
+            // Build database workers
+            await this.buildDatabaseWorkers(app, logWorker);
 
             // Build custom graph workers
             logWorker?.write(OmniHiveLogLevel.Info, `Master Web Process => Custom Graph Generation Started`);
@@ -613,4 +629,243 @@ export class ServerService {
 
         return contextModule.exports;
     };
+
+    private buildDatabaseWorkers = async (app: any, logWorker?: ILogWorker) => {
+        try {
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Connection Schemas Being Loaded`);
+
+            // Get build workers
+            const buildWorkers: RegisteredHiveWorker[] = [];
+
+            global.omnihive.registeredWorkers.forEach((worker: RegisteredHiveWorker) => {
+                if (worker.type === HiveWorkerType.GraphBuilder) {
+                    buildWorkers.push(worker);
+                }
+            });
+
+            // Get db workers
+            const dbWorkers: BuilderDatabaseWorker[] = [];
+
+            buildWorkers.forEach((worker: RegisteredHiveWorker) => {
+                const buildWorkerMetadata: HiveWorkerMetadataGraphBuilder =
+                    worker.metadata as HiveWorkerMetadataGraphBuilder;
+
+                if (buildWorkerMetadata.dbWorkers.includes("*")) {
+                    global.omnihive.registeredWorkers
+                        .filter((worker: RegisteredHiveWorker) => worker.type === HiveWorkerType.Database)
+                        .forEach((dbWorker: RegisteredHiveWorker) => {
+                            dbWorkers.push({ registeredWorker: dbWorker, builderName: worker.name });
+                        });
+                } else {
+                    buildWorkerMetadata.dbWorkers.forEach((value: string) => {
+                        const dbWorker: RegisteredHiveWorker | undefined = global.omnihive.registeredWorkers.find(
+                            (worker: RegisteredHiveWorker) =>
+                                worker.name === value && worker.type === HiveWorkerType.Database
+                        );
+                        if (!IsHelper.isNullOrUndefined(dbWorker)) {
+                            dbWorkers.push({ registeredWorker: dbWorker, builderName: worker.name });
+                        }
+                    });
+                }
+            });
+
+            // Write database schemas
+
+            for (const worker of dbWorkers) {
+                logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Retrieving ${worker.registeredWorker.name} Schema`);
+
+                const dbWorkerMeta = worker.registeredWorker.metadata as HiveWorkerMetadataDatabase;
+                const result: ConnectionSchema = await AwaitHelper.execute(
+                    (worker.registeredWorker.instance as IDatabaseWorker).getSchema()
+                );
+
+                result.tables.forEach((schema: TableSchema) => {
+                    if (dbWorkerMeta.ignoreSchema) {
+                        schema.tableNameCamelCase = camelCase(schema.tableName);
+                        schema.tableNamePascalCase = this.capitalizeFirstLetter(camelCase(schema.tableName));
+                    } else {
+                        schema.tableNameCamelCase = `${schema.schemaName.toLowerCase()}${this.capitalizeFirstLetter(
+                            camelCase(schema.tableName)
+                        )}`;
+                        schema.tableNamePascalCase = `${this.capitalizeFirstLetter(
+                            schema.schemaName.toLowerCase()
+                        )}${this.capitalizeFirstLetter(camelCase(schema.tableName))}`;
+                    }
+
+                    if (schema.columnIsForeignKey) {
+                        if (dbWorkerMeta.ignoreSchema) {
+                            schema.columnForeignKeyTableNameCamelCase = camelCase(schema.columnForeignKeyTableName);
+                            schema.columnForeignKeyTableNamePascalCase = this.capitalizeFirstLetter(
+                                camelCase(schema.columnForeignKeyTableName)
+                            );
+                        } else {
+                            schema.columnForeignKeyTableNameCamelCase = `${schema.schemaName.toLowerCase()}${this.capitalizeFirstLetter(
+                                camelCase(schema.columnForeignKeyTableName)
+                            )}`;
+                            schema.columnForeignKeyTableNamePascalCase = `${this.capitalizeFirstLetter(
+                                camelCase(schema.schemaName)
+                            )}${this.capitalizeFirstLetter(camelCase(schema.columnForeignKeyTableName))}`;
+                        }
+                    }
+
+                    let columnWorkingName = camelCase(schema.columnNameDatabase);
+
+                    columnWorkingName = columnWorkingName.replace(/[^a-zA-Z0-9 ]+/g, "");
+                    columnWorkingName = columnWorkingName.replace(/ /g, "_");
+                    columnWorkingName = columnWorkingName.charAt(0).toLowerCase() + columnWorkingName.slice(1);
+
+                    if (IsHelper.isNumber(schema.columnNameDatabase.substring(0, 1))) {
+                        columnWorkingName = "_N_" + columnWorkingName;
+                    }
+
+                    if (schema.columnNameDatabase.substring(0, 3) === "___") {
+                        columnWorkingName = "_3_" + columnWorkingName;
+                    } else if (schema.columnNameDatabase.substring(0, 2) === "__") {
+                        columnWorkingName = "_2_" + columnWorkingName;
+                    } else if (schema.columnNameDatabase.substring(0, 1) === "_") {
+                        columnWorkingName = "_1_" + columnWorkingName;
+                    }
+
+                    schema.columnNameEntity = columnWorkingName.toString();
+                });
+
+                global.omnihive.registeredSchemas.push({
+                    workerName: worker.registeredWorker.name,
+                    tables: result.tables,
+                    procFunctions: result.procFunctions,
+                });
+            }
+
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Connection Schemas Completed`);
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Writing Graph Generation Files`);
+
+            // Get all build workers and write out their graph schema
+            const dbWorkerModules: { dbWorkerName: string; builderWorkerName: string; dbModule: any }[] = [];
+
+            for (const builder of buildWorkers) {
+                logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Building Graph Build ${builder.name}`);
+
+                const buildWorker: IGraphBuildWorker = builder.instance as IGraphBuildWorker;
+
+                for (const dbWorker of dbWorkers.filter(
+                    (worker: BuilderDatabaseWorker) => worker.builderName === buildWorker.name
+                )) {
+                    const databaseWorker: IDatabaseWorker = dbWorker.registeredWorker.instance as IDatabaseWorker;
+                    const schema: ConnectionSchema | undefined = global.omnihive.registeredSchemas.find(
+                        (value: ConnectionSchema) => value.workerName === dbWorker.registeredWorker.name
+                    );
+
+                    const graphWorkerReturn = await buildWorker.buildDatabaseWorkerSchema(databaseWorker, schema);
+                    let dbWorkerModule = undefined;
+
+                    if (typeof graphWorkerReturn === "string") {
+                        dbWorkerModule = this.importFromString(graphWorkerReturn);
+                    } else {
+                        dbWorkerModule = {
+                            FederatedGraphQuerySchema: graphWorkerReturn,
+                        };
+                    }
+
+                    dbWorkerModules.push({
+                        dbWorkerName: dbWorker.registeredWorker.name,
+                        builderWorkerName: builder.name,
+                        dbModule: dbWorkerModule,
+                    });
+                }
+
+                logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Completed Graph Build ${builder.name}`);
+            }
+
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Completed Graph Generation Files`);
+            // Register graph builder databases
+
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Database Graph Endpoint Registering`);
+
+            for (const builder of buildWorkers) {
+                const builderMeta = builder.metadata as HiveWorkerMetadataGraphBuilder;
+
+                const builderDbWorkers = dbWorkers.filter(
+                    (worker: BuilderDatabaseWorker) => builder.name === worker.builderName
+                );
+
+                if (!IsHelper.isEmptyArray(builderDbWorkers)) {
+                    for (const databaseWorker of builderDbWorkers) {
+                        const dbWorkerMeta = databaseWorker.registeredWorker.metadata as HiveWorkerMetadataDatabase;
+                        let graphDatabaseSchema: any;
+
+                        const databaseDynamicModule: any = dbWorkerModules.filter(
+                            (value) =>
+                                value.dbWorkerName === databaseWorker.registeredWorker.name &&
+                                value.builderWorkerName === builder.name
+                        )[0].dbModule;
+                        const databaseQuerySchema: any = databaseDynamicModule.FederatedGraphQuerySchema;
+
+                        // eslint-disable-next-line prefer-const
+                        graphDatabaseSchema = databaseQuerySchema;
+
+                        logWorker?.write(
+                            OmniHiveLogLevel.Info,
+                            `Graph Progress => ${builder.name} => ${databaseWorker.registeredWorker.name} Query Schema Merged`
+                        );
+
+                        const procSchema: any = databaseDynamicModule.FederatedGraphProcSchema;
+
+                        if (!IsHelper.isNullOrUndefined(procSchema)) {
+                            graphDatabaseSchema = mergeSchemas({ schemas: [graphDatabaseSchema, procSchema] });
+                        }
+
+                        logWorker?.write(
+                            OmniHiveLogLevel.Info,
+                            `Graph Progress => ${builder.name} => ${databaseWorker.registeredWorker.name} Proc Schema Merged`
+                        );
+
+                        const graphDatabaseConfig: ApolloServerExpressConfig = {
+                            introspection: global.omnihive.getEnvironmentVariable<boolean>("OH_CORE_GRAPH_INTROSPECTION") ?? false,
+                            schema: graphDatabaseSchema,
+                            context: async ({ req }) => {
+                                const omnihive = {
+                                    access: req.headers["x-omnihive-access"] || ``,
+                                    auth: req.headers.authorization || ``,
+                                    cache: req.headers["x-omnihive-cache-type"] || ``,
+                                    cacheSeconds: req.headers["x-omnihive-cache-seconds"],
+                                };
+                                return { omnihive };
+                            },
+                        };
+
+                        const graphPlayground = global.omnihive.getEnvironmentVariable<boolean>("OH_CORE_GRAPH_PLAYGROUND") ?? true;
+
+                        if (graphPlayground) {
+                            graphDatabaseConfig.plugins?.push(
+                                ApolloServerPluginLandingPageGraphQLPlayground({
+                                    endpoint: `${this.webRootUrl}/${builderMeta.urlRoute}/${dbWorkerMeta.urlRoute}`,
+                                })
+                            );
+                        }
+
+                        const graphDatabaseServer: ApolloServer = new ApolloServer(graphDatabaseConfig);
+                        await AwaitHelper.execute(graphDatabaseServer.start());
+                        graphDatabaseServer.applyMiddleware({
+                            app,
+                            path: `/${builderMeta.urlRoute}/${dbWorkerMeta.urlRoute}`,
+                        });
+
+                        global.omnihive.registeredUrls.push({
+                            path: `${this.webRootUrl}/${builderMeta.urlRoute}/${dbWorkerMeta.urlRoute}`,
+                            type: RegisteredUrlType.GraphDatabase,
+                            metadata: {},
+                        });
+                    }
+                }
+            }
+
+            logWorker?.write(OmniHiveLogLevel.Info, `Graph Progress => Database Graph Endpoint Registered`);
+        } catch (err: any) {
+            logWorker?.write(OmniHiveLogLevel.Error, err.message);
+        }
+    }
+
+    private capitalizeFirstLetter(value: string) {
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    }
 }
